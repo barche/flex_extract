c00f2d52839dd9eef6f5fd75a3431491f4f12797
Katharina Baier <k.baier@univie.ac.at>
2022-01-31 10:40:23 +0000

    Update clear_ectrans_list_v2.py

--- clear_ectrans_list_v2.py
+++ clear_ectrans_list_v2.py
@@ -1,1 +1,24 @@
+"""
+run on ECMWF-Server: python clear_ectrans_list_v2.py
 
+package requirements: subprocess (pip install subprocess)
+
+original author: A. Plach, updated by K. Baier
+
+"""
+
+from subprocess import check_output#, run
+import subprocess
+# write output of ecaccess-ectrans-list to a variable, but transfer it
+# to a string (from bytes format) and split at newline
+
+out = str(check_output(['ecaccess-ectrans-list'])).split('\n')
+
+for i,line in enumerate(out):
+    line_tmp = line.split()
+    procID = line_tmp[0]
+    try:
+         subprocess.check_call(['ecaccess-ectrans-delete', str(procID)])
+         print(str(procID))
+    except TypeError as e:
+        print(e)
